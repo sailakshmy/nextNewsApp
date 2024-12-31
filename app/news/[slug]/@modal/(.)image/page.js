d8051276866fbdcdf02ec0dbf3dfad50b052b46a
sailakshmy <sailakshmy94@gmail.com>
2024-12-31 08:51:49 +0400

    Programmatic Navigation

--- app/news/[slug]/@modal/(.)image/page.js
+++ app/news/[slug]/@modal/(.)image/page.js
@@ -1,12 +1,15 @@
+"use client";
 import { DUMMY_NEWS } from "@/dummy-news";
+import { notFound, useRouter } from "next/navigation";
 const InterceptedImagePage = ({ params }) => {
+  const router = useRouter();
   const newsItem = DUMMY_NEWS?.find((news) => news?.slug === params?.slug);
   if (!newsItem) {
     notFound();
   }
   return (
     <>
-      <div className="modal-backdrop" />
+      <div className="modal-backdrop" onClick={router.back} />
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img src={`/images/news/${newsItem?.image}`} alt={newsItem?.title} />
