90f1eeba1f6e607bfd081edf0241c3ca5090900b
sailakshmy <sailakshmy94@gmail.com>
2025-01-01 17:43:31 +0400

    Created an API route Handler and a Middleware JS just for learning

---
+++ app/api/route.js
@@ -0,0 +1,8 @@
+export function GET(request) {
+  //   console.log("request from Get", request);
+  return new Response("Hello from GET");
+}
+
+// export function POST(request) {
+//   console.log("request from POST", request);
+// }
