bc904aea49707fcb4f456a8fed1f80102b01a07e
sailakshmy <sailakshmy94@gmail.com>
2025-01-02 06:58:58 +0400

    Server side data fetching

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,32 +1,38 @@
-"use client";
+// "use client";
 import NewsList from "@/components/NewsList/news-list";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-const News = () => {
-  const [loadingNews, setLoadingNews] = useState(false);
-  const [error, setError] = useState();
-  const [news, setNews] = useState();
-  useEffect(() => {
-    async function fetchNews() {
-      setLoadingNews(true);
-      const response = await fetch("http://localhost:8080/news");
-      if (!response.ok) {
-        setError("Failed to fetch news!");
-        setLoadingNews(false);
-      }
-      const newsList = await response.json();
-      setLoadingNews(false);
-      setNews(newsList);
-    }
-    fetchNews();
-  }, []);
+const News = async () => {
+  // const [loadingNews, setLoadingNews] = useState(false);
+  // const [error, setError] = useState();
+  // const [news, setNews] = useState();
+  // useEffect(() => {
+  //   async function fetchNews() {
+  //     setLoadingNews(true);
+  //     const response = await fetch("http://localhost:8080/news");
+  //     if (!response.ok) {
+  //       setError("Failed to fetch news!");
+  //       setLoadingNews(false);
+  //     }
+  //     const newsList = await response.json();
+  //     setLoadingNews(false);
+  //     setNews(newsList);
+  //   }
+  //   fetchNews();
+  // }, []);
+  const response = await fetch("http://localhost:8080/news");
+  const newsList = await response.json();
+  if (!response.ok) {
+    throw new Error("Failed to fetch news!");
+  }
   return (
     <>
       <div>
         <h1>News Page</h1>
-        {loadingNews && <p>Loading....</p>}
+        {newsList && <NewsList newsList={newsList} />}
+        {/* {loadingNews && <p>Loading....</p>}
         {error && <p>{error}</p>}
-        {news && <NewsList newsList={news} />}
+        {news && <NewsList newsList={news} />} */}
       </div>
     </>
   );
